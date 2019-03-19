31b29ae52ce4f8793dccb9552dd45f7feeb7a18e
shook <liushoukun66@gmail.com>
2019-03-19 15:10:05 +0800

    laravel-admin-ext/jxlwqq/env-manager

--- config/admin.php
+++ config/admin.php
@@ -315,6 +315,10 @@ return [
     |
     */
     'extensions' => [
+        'env-manager' => [
+            // If the value is set to false, this extension will be disabled
+            'enable' => true
+        ]
 
     ],
 ];
