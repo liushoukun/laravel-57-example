28489e75457666090f2242bcaf5157973167e2f1
shook <liushoukun66@gmail.com>
2019-03-19 15:06:40 +0800

    laravel-admin-ext/config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Encore\Admin\Config\Config;
 use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // 自定义配置
+        Config::load();
+
     }
 
     /**
